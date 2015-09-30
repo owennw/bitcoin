bc992365d566eb5441f5fc861fec244bdeaa62ed
Nick Owen <nowen@scottlogic.com>
2015-09-30 14:08:40 +0100

    Implement use strict

--- genesis_block.js
+++ genesis_block.js
@@ -1,107 +1,110 @@
-var crypto = require('crypto');
-var buffer = require('buffer');
-var https = require('https');
+(function() {
+	'use strict';
+	var crypto = require('crypto');
+	var buffer = require('buffer');
+	var https = require('https');
 
-var hash = process.argv[2];
-fetchAndVerifyBlock(hash);
+	var hash = process.argv[2];
+	fetchAndVerifyBlock(hash);
 
-function fetchAndVerifyBlock(h) {
-	var options = {
-		hostname: 'blockexplorer.com',
-		path: '/api/block/' + h
-	};
+	function fetchAndVerifyBlock(h) {
+		var options = {
+			hostname: 'blockexplorer.com',
+			path: '/api/block/' + h
+		};
 
-	var callback = function(res) {
-		var str = '';
-		res.on('data', function(d) {
-			str += d;
-		});
+		var callback = function(res) {
+			var str = '';
+			res.on('data', function(d) {
+				str += d;
+			});
 
-		res.on('end', function() {
-			var block = JSON.parse(str);
-			verify(block);
-		});
+			res.on('end', function() {
+				var block = JSON.parse(str);
+				verify(block);
+			});
+		}
+
+		https.request(options, callback).end();
 	}
 
-	https.request(options, callback).end();
-}
+	function verify(block) {
+		// The previous block hash may not be present
+		var previousBlockHash = block.previousblockhash == undefined ? 
+			'0000000000000000000000000000000000000000000000000000000000000000' : block.previousblockhash;
 
-function verify(block) {
-	// The previous block hash may not be present
-	var previousBlockHash = block.previousblockhash == undefined ? 
-		'0000000000000000000000000000000000000000000000000000000000000000' : block.previousblockhash;
+		// create the header value in hexadecimal
+		// Note: Bitcoin uses big-endian notation, so all the components must be converted
+		// to little-endian.
+		var hexHeader = 
+			swapEndian(numberToHex(block.version)) + 
+			swapEndian(previousBlockHash) + 
+			swapEndian(calculateMerkleRoot(block)) + 
+			swapEndian(numberToHex(block.time.toString(16))) + 
+			swapEndian(block.bits) + 
+			swapEndian(numberToHex(block.nonce.toString(16)));
 
-	// create the header value in hexadecimal
-	// Note: Bitcoin uses big-endian notation, so all the components must be converted
-	// to little-endian.
-	var hexHeader = 
-		swapEndian(numberToHex(block.version)) + 
-		swapEndian(previousBlockHash) + 
-		swapEndian(calculateMerkleRoot(block)) + 
-		swapEndian(numberToHex(block.time.toString(16))) + 
-		swapEndian(block.bits) + 
-		swapEndian(numberToHex(block.nonce.toString(16)));
+		var hash2 = doubleHash(hexHeader);
+		var finalHash = swapEndian(hash2);
 
-	var hash2 = doubleHash(hexHeader);
-	var finalHash = swapEndian(hash2);
-
-	// Finally verify the hash
-	console.log(finalHash === block.hash ? "The hash is correct!" : "The hash is incorrect!");
-}
-
-function doubleHash(hex) {
-	return hashHexToHex(hashHexToHex(hex));
-}
-
-function calculateMerkleRoot(block) {
-	var hashedTransactions = block.tx;
-	
-	while (hashedTransactions.length != 1) {
-		hashedTransactions = createNextTreeRow(hashedTransactions);
+		// Finally verify the hash
+		console.log(finalHash === block.hash ? "The hash is correct!" : "The hash is incorrect!");
 	}
 
-	// This is now the merkleroot
-	return hashedTransactions.toString();
-}
-
-function createNextTreeRow(currentRow) {
-	var nextLevel = [];
-	var size = currentRow.length;
-
-	if (size % 2 == 1) {
-		// There are an odd number of transactions, so the final hash is duplicated
-		currentRow.push(currentRow[size - 1]);
+	function doubleHash(hex) {
+		return hashHexToHex(hashHexToHex(hex));
 	}
 
-	for (i = 0; i < size; i += 2) {
-		var nextHash = swapEndian(doubleHash(swapEndian(currentRow[i]) + swapEndian(currentRow[i + 1])));
-		nextLevel.push(nextHash);
+	function calculateMerkleRoot(block) {
+		var hashedTransactions = block.tx;
+		
+		while (hashedTransactions.length != 1) {
+			hashedTransactions = createNextTreeRow(hashedTransactions);
+		}
+
+		// This is now the merkleroot
+		return hashedTransactions.toString();
 	}
 
-	return nextLevel;
-}
+	function createNextTreeRow(currentRow) {
+		var nextLevel = [];
+		var size = currentRow.length;
 
-function hashHexToHex(hex) {
-	var inputBuffer = new Buffer(hex, 'hex');
+		if (size % 2 == 1) {
+			// There are an odd number of transactions, so the final hash is duplicated
+			currentRow.push(currentRow[size - 1]);
+		}
 
-	var hash = crypto.createHash('sha256');
-	hash.update(inputBuffer);
-	return hash.digest('hex');
-}
+		for (var i = 0; i < size; i += 2) {
+			var nextHash = swapEndian(doubleHash(swapEndian(currentRow[i]) + swapEndian(currentRow[i + 1])));
+			nextLevel.push(nextHash);
+		}
 
-function toArray(s) {
-	var output = [];
-	for (var i = 0; i < s.length; i += 2) {
-		output.push(s.substr(i, 2));
+		return nextLevel;
 	}
-	return output;
-}
 
-function swapEndian(s) {
-	return toArray(s).reverse().join('');
-}
+	function hashHexToHex(hex) {
+		var inputBuffer = new Buffer(hex, 'hex');
 
-function numberToHex(n) {
-	// Format this number to have 8 digits
-	return ("0000000" + n.toString(16)).substr(-8);
-}
+		var hash = crypto.createHash('sha256');
+		hash.update(inputBuffer);
+		return hash.digest('hex');
+	}
+
+	function toArray(s) {
+		var output = [];
+		for (var i = 0; i < s.length; i += 2) {
+			output.push(s.substr(i, 2));
+		}
+		return output;
+	}
+
+	function swapEndian(s) {
+		return toArray(s).reverse().join('');
+	}
+
+	function numberToHex(n) {
+		// Format this number to have 8 digits
+		return ("0000000" + n.toString(16)).substr(-8);
+	}
+})();
